dd4b08e4fb28eb3f14be50f3adc80eb109a21803
Shicong Huang <hstonec@gmail.com>
2015-06-27 15:47:27 -0400

    init env.php

---
+++ libraries/env.php
@@ -0,0 +1,31 @@
+<?php
+session_set_cookie_params(300);
+session_start();
+
+function isLogin() {
+    if (isset($_SESSION["userID"]))
+        return true;
+    else
+        return false;
+}
+function login($userID) {
+    $_SESSION["userID"] = $userID;
+}
+function logout() {
+    unset($_SESSION["userID"]);
+    session_destroy();
+}
+function forward($relPath) {
+    if (gettype($relPath) != "string") {
+        echo "ERROR: forward() only accepts String!";
+        exit;
+    }
+    $appRootURL = "http://".$_SERVER["HTTP_HOST"];
+    if ($_SERVER["SERVER_PORT"] !== "80")
+        $appRootURL .= ":".$_SERVER["SERVER_PORT"];
+    $appRootURL .= pathinfo($_SERVER["PHP_SELF"])["dirname"]."/";
+    $newURL = $appRootURL.$relPath;
+    header("Location: ".$newURL);
+}
+
+?>
